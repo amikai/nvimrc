f930fd23651aee4f02f9ae3136288eacf0a0d1ac
amikai <amikai@localhost.localdomain>
2018-05-08 07:09:41 +0100

    ins: Add install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+vimrc_path=$HOME/.config/nvim
+current_dir=$(pwd)
+
+git clone https://github.com/amikai/nvimrc.git
+mv $current_dir/../nvimrc $vimrc_path
+mv $vimrc_path/nvimrc/init.vim $vimrc_path/init.vim
