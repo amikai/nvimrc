a67794b08118f53f83ba8bda6f08e430d35e021d
amikai <as23041248@gmail.com>
2018-05-08 07:12:23 +0100

    ins: install script online

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 vimrc_path=$HOME/.config/nvim
 current_dir=$(pwd)
 
+bash <(curl -s https://raw.githubusercontent.com/amikai/nvimrc/install/install.sh)
 git clone https://github.com/amikai/nvimrc.git
 mv $current_dir/../nvimrc $vimrc_path
 mv $vimrc_path/nvimrc/init.vim $vimrc_path/init.vim
