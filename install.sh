91b2fbd397af46c436483f44622ec4e9c13288f9
amikai <as23041248@gmail.com>
2018-05-08 07:22:15 +0100

    ins: Use mv init.vim will influence git status

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ current_dir=$(pwd)
 
 git clone https://github.com/amikai/nvimrc.git
 mv $current_dir/nvimrc $vimrc_path/nvimrc
-mv $vimrc_path/nvimrc/init.vim $vimrc_path/init.vim
+cp $vimrc_path/nvimrc/init.vim $vimrc_path/init.vim
