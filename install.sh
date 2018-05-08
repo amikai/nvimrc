bfccada2550903d0f8be82c5f9c98f195c4577e9
amikai <as23041248@gmail.com>
2018-05-08 07:18:30 +0100

    ins: Correct wrong path

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ vimrc_path=$HOME/.config/nvim
 current_dir=$(pwd)
 
 git clone https://github.com/amikai/nvimrc.git
-mv $current_dir/../nvimrc $vimrc_path
+mv $current_dir/nvimrc $vimrc_path
 mv $vimrc_path/nvimrc/init.vim $vimrc_path/init.vim
