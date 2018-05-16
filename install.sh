a786e41ff4d96f197c0a564ebc9fb612fe5da603
amikai <as23041248@gmail.com>
2018-05-16 15:29:44 +0800

    install: Change download path to /tmp

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 vimrc_path=$HOME/.config/nvim
 current_dir=$(pwd)
 
-git clone https://github.com/amikai/nvimrc.git
-mv $current_dir/nvimrc $vimrc_path/nvimrc
-cp $vimrc_path/nvimrc/init.vim $vimrc_path/init.vim
+git clone https://github.com/amikai/nvimrc.git /tmp/nvimrc
+mv tmp/nvimrc $vimrc_path/nvimrc
+cp tmp/nvimrc/init.vim $vimrc_path/init.vim
 
 # install vim-plug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
