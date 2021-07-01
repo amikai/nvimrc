33095c6e5ca3c4dd8cb674439d24bd4de2ce23b5
amikai chuang (莊雲凱) <amikai_chuang@htc.com>
2021-07-01 12:00:03 +0800

    Implement utility function autocmd and keymap

---
+++ lua/my_config/utils.lua
@@ -0,0 +1,22 @@
+local M = {}
+
+local cmd = vim.cmd
+
+-- Copy from https://github.com/wbthomason/dotfiles/blob/387ded8ad4c3cb9d5000edbd3b18bc8cb8a186e9/neovim/.config/nvim/lua/config/utils.lua
+function M.autocmd(group, cmds, clear)
+  clear = clear == nil and false or clear
+  if type(cmds) == 'string' then cmds = {cmds} end
+  cmd('augroup ' .. group)
+  if clear then cmd [[au!]] end
+  for _, c in ipairs(cmds) do cmd('autocmd ' .. c) end
+  cmd [[augroup END]]
+end
+
+function M.km(modes, lhs, rhs, opts)
+  opts = opts or {}
+  opts.noremap = opts.noremap == nil and true or opts.noremap
+  if type(modes) == 'string' then modes = {modes} end
+  for _, mode in ipairs(modes) do map_key(mode, lhs, rhs, opts) end
+end
+
+return M
