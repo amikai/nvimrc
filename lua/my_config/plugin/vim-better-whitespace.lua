d7a3cb8dccd3edee2044bf90ec1e4d290f18d84d
amikai chuang (莊雲凱) <amikai_chuang@htc.com>
2021-07-18 21:00:11 +0800

    Add fern and tagbar in better-whitespace blacklist

--- lua/my_config/plugin/vim-better-whitespace.lua
+++ lua/my_config/plugin/vim-better-whitespace.lua
@@ -7,7 +7,7 @@ function M.setup()
     g.better_whitespace_enabled = 1
     g.better_whitespace_ctermcolor ='3'
     g.better_whitespace_guicolor = '#c8e6c9'
-    g.better_whitespace_filetypes_blacklist = {'nerdtree', 'help', 'qf', 'diff'}
+    g.better_whitespace_filetypes_blacklist = {'nerdtree', 'help', 'qf', 'diff', 'tagbar', 'fern'}
 
     km('n', '<F5>', '<cmd>ToggleWhitespace<cr>', {noremap = true})
 end
