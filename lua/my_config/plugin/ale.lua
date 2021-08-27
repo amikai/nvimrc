1a6d0df5755946fc6feebc63680c732d409c9d5c
amikai chuang (莊雲凱) <amikai_chuang@htc.com>
2021-08-27 21:52:17 +0800

    ALE sign setting

--- lua/my_config/plugin/ale.lua
+++ lua/my_config/plugin/ale.lua
@@ -1,7 +1,6 @@
 local M = {}
 
 local g = vim.g
-local autocmd = require('my_config.utils').autocmd
 
 function M.setup()
     g.ale_completion_enabled = 0
@@ -9,9 +8,9 @@ function M.setup()
     g.ale_set_quickfix = 0
     g.ale_set_loclist = 1
 
-    g.ale_sign_error = 'E'
-    g.ale_sign_warning = 'W'
-    g.ale_sign_info = 'I'
+    g.ale_sign_error = '✗'
+    g.ale_sign_warning = '!'
+    g.ale_sign_info = ''
 
     g.ale_lint_on_text_changed = 1
     g.ale_lint_delay = 500
