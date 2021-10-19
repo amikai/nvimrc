a49b6344f8088a075dfc4b0b3ba5bb52bba67989
amikai chuang (莊雲凱) <amikai_chuang@htc.com>
2021-10-19 22:26:47 +0800

    disable treesitter vim highlight

--- lua/my_config/plugin/nvim-treesitter.lua
+++ lua/my_config/plugin/nvim-treesitter.lua
@@ -17,6 +17,7 @@ function M.config()
         },
         highlight = {
             enable = true,
+            additional_vim_regex_highlighting = false,
         },
         indent = {
             endble = true
