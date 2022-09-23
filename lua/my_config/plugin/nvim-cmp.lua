7c4bd3f1ebbd79e1fc34ce6bfab0cd2245fdc591
amikai <as23041248@gmail.com>
2022-09-23 16:40:18 +0800

    do not jump or expand when auto complete

--- lua/my_config/plugin/nvim-cmp.lua
+++ lua/my_config/plugin/nvim-cmp.lua
@@ -24,8 +24,6 @@ function M.config()
 			["<Tab>"] = cmp.mapping(function(fallback)
 				if cmp.visible() then
 					cmp.select_next_item()
-				elseif vim.fn["vsnip#available"](1) == 1 then
-					feedkey("<Plug>(vsnip-expand-or-jump)", "")
 				elseif has_words_before() then
 					cmp.complete()
 				else
@@ -36,8 +34,6 @@ function M.config()
 			["<S-Tab>"] = cmp.mapping(function()
 				if cmp.visible() then
 					cmp.select_prev_item()
-				elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-					feedkey("<Plug>(vsnip-jump-prev)", "")
 				end
 			end, { "i", "s" }),
 			["<CR>"] = cmp.mapping.confirm({ select = true }),
