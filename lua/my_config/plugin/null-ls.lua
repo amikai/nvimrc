2ebe3c317a0716febecb30f54fbede598c810aae
amikai <as23041248@gmail.com>
2022-11-08 22:03:11 +0800

    keymap for opening diagnostic window and formatting

--- lua/my_config/plugin/null-ls.lua
+++ lua/my_config/plugin/null-ls.lua
@@ -1,4 +1,5 @@
 local M = {}
+local utils = require("my_config.utils")
 
 local null_ls = require("null-ls")
 
@@ -17,6 +18,9 @@ function M.config()
             null_ls.builtins.formatting.buf,
         },
     })
+
+    vim.keymap.set("n", "<F9>", utils.toggle_diagnostic_window)
+    vim.keymap.set("n", "<F3>", vim.lsp.buf.format)
 end
 
 return M
