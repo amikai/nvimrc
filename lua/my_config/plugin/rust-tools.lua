1d41c3492ad329349ef3aadfc7db81f9b9bc080a
amikai <as23041248@gmail.com>
2023-01-18 14:20:04 +0800

    add clippy for linting

--- lua/my_config/plugin/rust-tools.lua
+++ lua/my_config/plugin/rust-tools.lua
@@ -14,6 +14,17 @@ function M.config()
                 km("n", "<Leader>a", rt.code_action_group.code_action_group)
             end,
             capabilities = my_lsp.capabilities,
+            settings = {
+                ['rust-analyzer'] = {
+                    checkOnSave = {
+                        allFeatures = true,
+                        overrideCommand = {
+                            'cargo', 'clippy', '--workspace', '--message-format=json',
+                            '--all-targets', '--all-features'
+                        }
+                    }
+                }
+            }
         },
     })
 end
