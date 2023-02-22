512ae631d17b0f487f76ad4e5a07938c2c2510a5
amikai <as23041248@gmail.com>
2023-02-22 13:47:13 +0800

    ignore node_modules and vendor when telescope find string

--- lua/my_config/plugin/telescope.lua
+++ lua/my_config/plugin/telescope.lua
@@ -9,6 +9,12 @@ end
 
 function M.config()
     require('telescope').setup {
+        defaults = {
+            file_ignore_patterns = {
+                "node_modules",
+                "vendor"
+            }
+        },
         extensions = {
             fzf = {
                 fuzzy = true, -- false will only do exact matching
