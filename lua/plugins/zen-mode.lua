dd7acfecf9cf2f2a3b49498fe7ec7962d7230787
amikai <as23041248@gmail.com>
2023-02-28 22:20:23 +0800

    update twilight setting

--- lua/plugins/zen-mode.lua
+++ lua/plugins/zen-mode.lua
@@ -49,8 +49,6 @@ return {
                 expand = { -- for treesitter, we we always try to expand to the top-most ancestor with these types
                     "function",
                     "method",
-                    "table",
-                    "if_statement",
                 },
                 exclude = {}, -- exclude these filetypes
             }
