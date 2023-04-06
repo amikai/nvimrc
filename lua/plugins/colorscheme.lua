a2f7522e9540402bcf2041ed784217d595280055
amikai <as23041248@gmail.com>
2023-04-06 23:55:57 +0800

    add new colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -22,4 +22,12 @@ return {
             -- vim.cmd([[colorscheme vscode]])
         end,
     },
+    {
+        "mcchrish/zenbones.nvim",
+        -- Optionally install Lush. Allows for more configuration or extending the colorscheme
+        -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
+        -- In Vim, compat mode is turned on as Lush only works in Neovim.
+        dependencies = "rktjmp/lush.nvim"
+    },
+    { "EdenEast/nightfox.nvim" },
 }
