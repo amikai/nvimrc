25e578844ebf199c7672ce4b11b6e9aa5917f17a
amikai <as23041248@gmail.com>
2023-04-11 20:05:31 +0800

    update gitsign setting

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -3,23 +3,6 @@ return {
     config = function()
         local gs = require("gitsigns")
         gs.setup({
-            signs = {
-                add = { hl = "GitSignsAdd", text = "+", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-                change = { hl = "GitSignsChange", text = "~", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-                delete = { hl = "GitSignsDelete", text = "_", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-                topdelete = {
-                    hl = "GitSignsDelete",
-                    text = "‾",
-                    numhl = "GitSignsDeleteNr",
-                    linehl = "GitSignsDeleteLn",
-                },
-                changedelete = {
-                    hl = "GitSignsChange",
-                    text = "│",
-                    numhl = "GitSignsChangeNr",
-                    linehl = "GitSignsChangeLn",
-                },
-            },
             on_attach = function(bufnr)
                 local function map(mode, l, r, opts)
                     opts = opts or {}
