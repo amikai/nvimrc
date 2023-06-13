59ff0082bad1e6812e155764503ab82ca9d0d93b
amikai <as23041248@gmail.com>
2023-06-13 21:54:38 +0800

    change symbol-outline keymap

--- lua/plugins/symbols-outline.lua
+++ lua/plugins/symbols-outline.lua
@@ -3,6 +3,18 @@ return {
         "simrat39/symbols-outline.nvim",
         opts = {
             autofold_depth = 1,
+            keymaps = { -- These keymaps can be a string or a table for multiple keys
+                close = { "<Esc>", "q" },
+                goto_location = "<Cr>",
+                focus_location = "o",
+                hover_symbol = "K",
+                toggle_preview = "p",
+                fold = "zc",
+                unfold = "zo",
+                fold_all = "zR",
+                unfold_all = "zM",
+                fold_reset = "R",
+            },
         },
         keys = {
             { "<F8>", "<cmd>SymbolsOutline<cr>", mode = "n" },
