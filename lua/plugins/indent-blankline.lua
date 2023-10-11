9843e699728167ed89e5ec4d13032b9b348ff106
amikai <as23041248@gmail.com>
2023-10-11 23:55:38 +0800

    update indent-blankline setting to newer version

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,17 +1,17 @@
 return {
     {
         "lukas-reineke/indent-blankline.nvim",
+        main = "ibl",
         config = function()
             vim.g.indent_blankline_disable_warning_message = true
             local exclude_ft = { "help", "git", "diff", "NvimTree", "vista" }
-            require("indent_blankline").setup({
+            require("ibl").setup({
                 -- U+2502 may also be a good choice, it will be on the middle of cursor.
                 -- U+250A is also a good choice
-                char = "▏",
-                show_end_of_line = true,
-                disable_with_nolist = true,
-                filetype_exclude = exclude_ft,
-                use_treesitter = true,
+                indent = { char = "▏" },
+                exclude = {
+                    filetypes = exclude_ft,
+                },
             })
 
             local gid = vim.api.nvim_create_augroup("indent_blankline", { clear = true })
@@ -19,7 +19,7 @@ return {
             autocmd("InsertEnter", {
                 pattern = "*",
                 group = gid,
-                command = "IndentBlanklineDisable",
+                command = "IBLEnable",
             })
 
             autocmd("InsertLeave", {
@@ -27,7 +27,7 @@ return {
                 group = gid,
                 callback = function()
                     if not vim.tbl_contains(exclude_ft, vim.bo.filetype) then
-                        vim.cmd([[IndentBlanklineEnable]])
+                        vim.cmd([[IBLDisable]])
                     end
                 end,
             })
