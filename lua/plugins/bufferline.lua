bbd8a63cdeb4661f47b38880df22170b72158266
amikai <as23041248@gmail.com>
2023-10-19 01:30:24 +0800

    use offset for nvimtree and symbol-outline in bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -3,7 +3,24 @@ return {
         "akinsho/bufferline.nvim",
         dependencies = { "nvim-tree/nvim-web-devicons" },
         config = function()
-            require("bufferline").setup({})
+            require("bufferline").setup({
+                options = {
+                    offsets = {
+                        {
+                            filetype = "NvimTree",
+                            text = "File Explorer",
+                            text_align = "center",
+                            separator = true
+                        },
+                        {
+                            filetype = "Outline",
+                            text = "Outliner",
+                            text_align = "center",
+                            separator = true
+                        }
+                    },
+                }
+            })
         end,
     },
 }
