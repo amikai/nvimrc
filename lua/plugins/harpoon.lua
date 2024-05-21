bd7b4d905f5132176ddc6b6da9eb8ae8dad1f671
amikai <as23041248@gmail.com>
2024-05-21 22:41:56 +0800

    update harpoon to v2

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,12 +1,13 @@
 return {
     {
         "ThePrimeagen/harpoon",
+        branch = "harpoon2",
         dependencies = { "nvim-lua/plenary.nvim" },
         keys = {
-            { "<leader><leader>", [[ <cmd>lua require("harpoon.ui").toggle_quick_menu()<cr> ]], mode = "n", { silent = true } },
-            { "<leader>a", [[ <cmd>lua require("harpoon.mark").add_file()<cr> ]], mode = "n", { silent = true } },
-            { "[h", [[ <cmd>lua require("harpoon.ui").nav_prev()<cr> ]], mode = "n", { silent = true } },
-            { "]h", [[ <cmd>lua require("harpoon.ui").nav_next()<cr> ]], mode = "n", { silent = true } },
+            { "<leader><leader>", [[ <cmd>lua require("harpoon").ui:toggle_quick_menu(require("harpoon"):list())<cr> ]], mode = "n", { silent = true } },
+            { "<leader>a", [[ <cmd>lua require("harpoon"):list():add()<cr> ]], mode = "n", { silent = true } },
+            { "[h", [[ <cmd>lua require("harpoon"):list():prev()<cr> ]], mode = "n", { silent = true } },
+            { "]h", [[ <cmd>lua require("harpoon"):list():next()<cr> ]], mode = "n", { silent = true } },
         },
     },
 }
