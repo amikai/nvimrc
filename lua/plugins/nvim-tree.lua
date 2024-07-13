0650563a7b3a40b4ee8b6add0ec767d750c1ce02
amikai <as23041248@gmail.com>
2024-07-13 09:07:58 +0800

    feat: ignore target folder in rust project

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -12,7 +12,19 @@ return {
         require("nvim-tree").setup {
             git = {
                 enable = false,
-            }
+            },
+            filesystem_watchers = {
+                ignore_dirs = function(path)
+                    -- ignore the target folder in rust folder
+                    -- nvim-tree watches for changes in all files. When
+                    -- rust-analyzer is running, it changes a bunch of files in
+                    -- its target folder, uses a lot of memory, and slows down
+                    -- the computer.
+                    if vim.fn.getcwd() .. "/Cargo.toml" and path == vim.fn.getcwd() .. "/target" then
+                        return true
+                    end
+                end
+            },
         }
     end,
 }
