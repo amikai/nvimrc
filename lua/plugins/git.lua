8cecf5c69627d11ae6d1eb69db54d8cc28027dbb
amikai <as23041248@gmail.com>
2024-10-07 13:42:47 +0800

    feat: use Gbrowse open company github web page

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,7 +1,13 @@
 return {
     {
         "tpope/vim-fugitive",
-        cmd = { "Git", "Gdiffsplit" },
+        cmd = { "G", "Git", "Gdiffsplit", "GBrowse" },
+    },
+    {
+        "tpope/vim-rhubarb",
+        init = function()
+            vim.g.github_enterprise_urls = { 'https://adc.github.trendmicro.com' }
+        end,
     },
     {
         "rbong/vim-flog",
