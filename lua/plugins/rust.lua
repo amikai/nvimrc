349e449cebc970762f3120842fd92f7b21e7c613
amikai <as23041248@gmail.com>
2024-10-11 01:50:30 +0800

    fix: lazy doesn't have 'setup' spec

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -3,7 +3,7 @@ return {
         'mrcjkb/rustaceanvim',
         version = '^5', -- Recommended
         lazy = false,   -- This plugin is already lazy
-        setup = function()
+        init = function()
             vim.g.rustaceanvim = {
                 -- Plugin configuration
                 tools = {
@@ -37,7 +37,7 @@ return {
                         local mason_registry = require('mason-registry')
                         local ra_binary = mason_registry.is_installed('rust-analyzer')
                             -- This may need to be tweaked, depending on the operating system.
-                            and mason_registry.get_package('rust-analyzer'):get_install_path() .. "/rust-analyzer"
+                            and require('mason.settings').current.install_root_dir .. "/bin/rust-analyzer"
                             or "rust-analyzer"
                         return { ra_binary } -- You can add args to the list, such as '--log-file'
                     end,
@@ -51,6 +51,7 @@ return {
                                 prefix = "crate",
                             },
                             checkOnSave = true,
+
                         },
                     },
                 },
