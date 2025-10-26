58429cbbff8101276edadf5e9cd826072b1ce26f
amikai <amikai.chuang@gmail.com>
2025-10-26 16:01:06 +0800

    code format

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -91,7 +91,7 @@ return {
         "HiPhish/rainbow-delimiters.nvim",
         dependencies = "nvim-treesitter/nvim-treesitter",
         config = function()
-            require ('rainbow-delimiters.setup').setup {}
+            require('rainbow-delimiters.setup').setup {}
         end
     },
     {
