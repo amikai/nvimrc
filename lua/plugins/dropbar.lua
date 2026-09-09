8def3f3617ddc3a084bbf53dd6326684db75670b
amikai <amikai.chuang@gmail.com>
2026-09-09 20:09:41 +0800

    feat(ui): add dropbar.nvim

---
+++ lua/plugins/dropbar.lua
@@ -0,0 +1,9 @@
+return {
+    {
+        "Bekaboo/dropbar.nvim",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+            "nvim-telescope/telescope-fzf-native.nvim",
+        },
+    },
+}
